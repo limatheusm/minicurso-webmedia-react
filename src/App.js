f2579db962fb6a5317649d7554cc77afbe8609db
Matheus Lima <matheus.lima@lavid.ufpb.br>
2018-09-19 15:36:21 -0300

    #1 - Adiciona lista de tarefas

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+class TaskItem extends React.Component {
+	render() {
+		return <li onClick={this.props.onClick}>{this.props.task.title}</li>;
+	}
+}
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+	state = { inputValue: '', tasks: [] };
+
+	onChangeInputValue = (event) => {
+		this.setState({ inputValue: event.target.value });
   }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.addTask();
+  }
+
+	addTask = () => {
+		const tasks = this.state.tasks;
+    const task = { title: this.state.inputValue };
+		tasks.push(task);
+		this.setState({ tasks: tasks });
+	};
+
+  // ... App component ...
+	removeTask = (index) => {
+		const tasks = this.state.tasks;
+		tasks.splice(index, 1);
+		this.setState({ tasks: tasks });
+	};
+
+	renderTasks = () => {
+		return (
+			<ul>
+				{this.state.tasks.map((task, index) => (
+					<TaskItem onClick={() => this.removeTask(index)} key={index} task={task} />
+				))}
+			</ul>
+		);
+	};
+  // ...
+	render() {
+		return (
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Tarefa:
+						<input type="text" value={this.state.inputValue} onChange={this.onChangeInputValue} />
+					</label>
+					<input type="submit" value="Adicionar" />
+        </form>
+        {this.renderTasks()}
+			</div>
+		);
+	}
 }
 
 export default App;
